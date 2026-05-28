2e68c6a26a6b0f52298536faa577486290587533
Mihhail Zolotarjov <misha.zolotarjov@gmail.com>
2026-05-28 22:26:49 +0300

    Add .gitignore and clean up old project folders

--- 03_20.02-klassikomplekt/decathlon/src/main/java/ee/mihhail/decathlon/repository/AthleteRepository.java
+++
@@ -1,7 +0,0 @@
-package ee.mihhail.decathlon.repository;
-
-import ee.mihhail.decathlon.entity.Athlete;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AthleteRepository extends JpaRepository<Athlete, Long> {
-}
--- demo/src/main/java/ee/mihhail/demo/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package ee.mihhail.demo.repository;
-
-import ee.mihhail.demo.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Movie, Long> {
-}
